4c63f6217d09ccb056cc890adf6679e59c9a29da
xinggang <xinggang.china@gmail.com>
2021-04-21 16:47:11 +0800

    🎉新增：spring-boot-demo-jdbctemplate 数据库访问

---
+++ spring-boot-demo-jdbctemplate/src/test/java/cn/lttc/jdbctemplatedemo/JdbcTemplateDemoTests.java
@@ -0,0 +1,13 @@
+package cn.lttc.jdbctemplatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateDemoTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
