265251150f50ecf23b6ddd197d8c8e4f06c8d3d3
xinggang <xinggang.china@gmail.com>
2021-04-21 16:47:11 +0800

    🎉新增：spring-boot-demo-jdbctemplate 数据库访问

---
+++ spring-boot-demo-jdbctemplate/src/main/java/cn/lttc/jdbctemplatedemo/JdbcTemplateDemo.java
@@ -0,0 +1,19 @@
+package cn.lttc.jdbctemplatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * JdbcTemplate 编程演示
+ *
+ * @author xinggang
+ * @create 2021-04-21
+ */
+@SpringBootApplication
+public class JdbcTemplateDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcTemplateDemo.class, args);
+    }
+
+}
