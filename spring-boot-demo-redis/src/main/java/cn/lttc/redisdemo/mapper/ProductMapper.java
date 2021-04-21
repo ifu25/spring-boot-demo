7a04de06fea3899dbe5665c42461c279b8f0dab5
sunjian <sj@lttc.com.cn>
2021-04-21 14:13:11 +0800

    ✨优化：springboot-redis demo mapper添加Repository注解

--- spring-boot-demo-redis/src/main/java/cn/lttc/redisdemo/mapper/ProductMapper.java
+++ spring-boot-demo-redis/src/main/java/cn/lttc/redisdemo/mapper/ProductMapper.java
@@ -1,9 +1,10 @@
 package cn.lttc.redisdemo.mapper;
 
 import cn.lttc.redisdemo.model.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ProductMapper {
     // 根据产品id获取产品
     Product findProductById(Integer id);
